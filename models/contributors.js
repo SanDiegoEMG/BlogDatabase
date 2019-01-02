bdc7294fda69f1cc819fea795610814185b3884b
SD_emg <sandiegoemg@gmail.com>
2019-01-02 15:25:45 -0800

    added validation to contributors and table for posts

--- models/contributors.js
+++ models/contributors.js
@@ -1,10 +1,35 @@
-module.exports = function (sequelize, DataTypes){
-    var Contributor = sequelize.define("Contributor",{
+module.exports = function (sequelize, DataTypes) {
+    var Contributor = sequelize.define("Contributor", {
         email: DataTypes.STRING,
-        first_name: DataTypes.STRING,
-        last_name: DataTypes.STRING,
-        screen_name: DataTypes.STRING,
-        goal_yearly_posts: DataTypes.INTEGER
-    })
+        first_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+
+            validate: {
+                len: [1, 25]
+            }
+        },
+        last_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+
+            validate: {
+                len: [1, 30]
+            },
+        },
+        screen_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1, 12]
+            }
+        },
+        goal_yearly_posts: {
+            type: DataTypes.INTEGER,
+            validate: {
+                len: [1, 700]
+            }
+        },
+    });
     return Contributor;
-}
+    };
