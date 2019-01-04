fee6f3ec5774c41ee2fb58bb9c3ae1363db56459
SD_emg <sandiegoemg@gmail.com>
2019-01-03 18:12:14 -0800

    less fields in models for easier testing

--- models/contributors.js
+++ models/contributors.js
@@ -8,21 +8,21 @@ module.exports = function (sequelize, DataTypes) {
                 len: [1, 25]
             }
         },
-        last_name: {
-            type: DataTypes.STRING,
-            allowNull: false,
+        // last_name: {
+        //     type: DataTypes.STRING,
+        //     allowNull: false,
 
-            validate: {
-                len: [1, 30]
-            },
-        },
-        screen_name: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            validate: {
-                len: [1, 12]
-            }
-        },
+        //     validate: {
+        //         len: [1, 30]
+        //     },
+        // },
+        // screen_name: {
+        //     type: DataTypes.STRING,
+        //     allowNull: false,
+        //     validate: {
+        //         len: [1, 12]
+        //     }
+        // },
         goal_yearly_posts: {
             type: DataTypes.INTEGER,
             validate: {
